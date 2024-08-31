08c4c7b10189339c57048b64e8c4b2c9313660e4
mateo vincitorio <mateovincitorio@gmail.com>
2024-08-31 11:49:57 -0300

    entrega 2

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
 let mapa = document.getElementById("mapa");
 let enlaces = document.querySelectorAll("#enlace1, #enlace2, #enlace3");
-let luna = document.getElementById("luna");
 const modoOscuro = document.getElementById("darkMode");
 localStorage.setItem('darkMode', 'activo')
 
@@ -9,13 +8,11 @@ if(localStorage.getItem("darkMode") === "activo"){
 }
 
 modoOscuro.addEventListener("click", () =>{
-    luna.innerHTML = `<h2>Modo oscuro inactivo</h2>`
     if (document.body.classList.contains("dark-Mode")) {
         document.body.classList.remove("dark-Mode")
         localStorage.setItem("darkMode", "inactivo");
     }else{
         document.body.classList.add("dark-Mode")
-        luna.innerHTML = `<h2>Modo oscuro activo</h2>`
         localStorage.setItem("darkMode", "activo")
     }
 })
