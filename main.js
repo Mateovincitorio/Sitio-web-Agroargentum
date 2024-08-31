750d44fa8c929e05cc2e6022ca0cc0f4e31ea20c
mateo vincitorio <mateovincitorio@gmail.com>
2024-08-31 11:27:31 -0300

    new commit

--- main.js
+++ main.js
@@ -1,61 +1,21 @@
-
-
-/*document.getElementById("darkMode").onclick = function(){
-    modoscuro();
-}*/
-let body = document.body;
 let mapa = document.getElementById("mapa");
 let enlaces = document.querySelectorAll("#enlace1, #enlace2, #enlace3");
+let luna = document.getElementById("luna");
 const modoOscuro = document.getElementById("darkMode");
-localStorage.setItem('darkMode', 'inactivo')
+localStorage.setItem('darkMode', 'activo')
 
-if(localStorage.getItem("darkMode") === "inactivo"){
-    document.body.classList.remove("dark-Mode")
-}else{
+if(localStorage.getItem("darkMode") === "activo"){
     document.body.classList.add("dark-Mode")
 }
 
 modoOscuro.addEventListener("click", () =>{
+    luna.innerHTML = `<h2>Modo oscuro inactivo</h2>`
     if (document.body.classList.contains("dark-Mode")) {
         document.body.classList.remove("dark-Mode")
         localStorage.setItem("darkMode", "inactivo");
     }else{
         document.body.classList.add("dark-Mode")
-        
+        luna.innerHTML = `<h2>Modo oscuro activo</h2>`
         localStorage.setItem("darkMode", "activo")
     }
 })
-
-/*function modoscuro() {
-    //selecciono el elemento body
-    
-/*
-    if (body.classList.contains("dark-mode")) {
-        body.classList.remove("dark-mode")
-        body.style.backgroundColor = "white";
-        body.style.color = "black";
-        body.style.transition = "background-color 0.5s ease, color 0.5s ease";
-        mapa.style.color = 'black';
-        enlaces.forEach(el=>{
-            el.style.color = "blanchedalmond"
-        })
-    }else{
-        body.classList.add("dark-mode")
-        body.style.backgroundColor = "black";
-        body.style.color = "blanchedalmond";
-        body.style.transition = "background-color 0.5s ease, color 0.5s ease";
-        mapa.style.color = 'blanchedalmond';
-        enlace1.style.color = 'blanchedalmond';
-        enlace2.style.color = 'blanchedalmond';
-        enlace3.style.color = 'blanchedalmond';
-    }*/
-    /*
-    // Modifica la propiedad CSS
-        body.style.backgroundColor = "black";
-        body.style.color = "blanchedalmond";
-        body.style.transition = "background-color 0.5s ease, color 0.5s ease";
-        mapa.style.color = 'blanchedalmond';
-        enlace1.style.color = 'blanchedalmond';
-        enlace2.style.color = 'blanchedalmond';
-        enlace3.style.color = 'blanchedalmond';*/
-
